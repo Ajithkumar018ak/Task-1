ccc1e45c24609c08fb83da1baec5ef1d036f3084
Ajithkumar018ak <kathiresanajith018@gmail.com>
2026-04-09 21:46:38 +0530

    commit

---
+++ task2.js
@@ -0,0 +1,154 @@
+//1. Print name & designation
+console.log("Name: Ajith");
+console.log("Designation: Front end Developer");
+
+// 2. Alert
+alert("Welcome to JavaScript Session"); 
+
+// 3. Confirm
+let likeCoding = confirm("Do you like coding?");
+console.log("Likes Coding:", likeCoding); 
+
+// 4. Prompt
+let favFood = prompt("Enter your favorite food:");
+console.log("Favorite Food:", favFood);
+
+// 5. Display in UI
+document.writeln("Good Evening Team");
+ 
+// console method
+console.log(100);
+
+console.warn("This is a warning");
+
+console.error("Something went wrong!");
+
+console.log("Message 1");
+console.log("Message 2");
+console.log("Message 3");
+
+console.clear();
+
+// data types
+let name = "Ajith";
+console.log(typeof name); 
+
+let age = 22;
+console.log(typeof age);
+
+let Student = true;
+console.log(Student); 
+
+let x;
+console.log(x); 
+
+let y = null;
+console.log(y); 
+
+//Arrays
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+console.log(fruits);
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+
+fruits.push("Pineapple");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(fruits.length);
+
+//objects
+
+let student = {
+    name: "Ajith",
+    age: 22,
+    course: "B.Tech IT",
+    Hobbies: ["cricket", "Reading book", "Editing"]
+};
+
+
+console.log(student.name);
+
+
+student.college = "HICET College";
+console.log(student);
+
+
+console.log(student.Hobbies[1]);
+
+
+student.age = 23;
+console.log(student);
+
+// operators
+
+let a = 4;
+let b = 2;
+
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a % b); 
+console.log(a ** b); 
+
+// increment & Decrement
+
+let num1 = 5;
+console.log(num1++); 
+console.log(num1);   
+
+let num2 = 5;
+console.log(++num2); 
+
+
+let x1 = 5;
+console.log(x1++); 
+console.log(x1);   
+
+let x2 = 5;
+console.log(++x2); 
+
+
+let d = 10;
+console.log(d--); 
+console.log(d);  
+
+//Logic 
+let userAge = prompt("Enter your age:");
+if (userAge >= 18) {
+    console.log("Eligible to vote");
+} else {
+    console.log("Not eligible");
+}
+
+let userName = prompt("Enter your name:");
+console.log("Hello " + userName);
+
+
+let marks = [85, 90, 78, 92, 88];
+let max = Math.max(...marks);
+console.log("Highest Mark:", max);
+
+
+let fruitObj = {
+    category: "Tropical",
+    items: ["Mango", "Banana", "Pineapple"]
+};
+
+console.log(fruitObj.items[0]);
+
+
+let favFruits = [];
+
+favFruits.push(prompt("Enter fruit 1:"));
+favFruits.push(prompt("Enter fruit 2:"));
+favFruits.push(prompt("Enter fruit 3:"));
+
+console.log(favFruits); 
